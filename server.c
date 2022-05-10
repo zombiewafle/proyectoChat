1f2ea66b56f6fe24154c9868283160492b9a62dc
Javier Salazar <javierandre117@gmail.com>
2022-05-09 22:36:36 -0600

    Initial commit

---
+++ server.c
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+
